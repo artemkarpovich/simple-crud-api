a7dbf8fe80bb05f679e88e18796b133ab86c43ac
Artsiom <karpartem94@gmail.com>
2021-11-25 09:46:30 +0300

    feat: add HTTP_METHODS + HTTP_STATUS_CODES constants

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const HTTP_METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
+
+const HTTP_STATUS_CODES = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = {
+  HTTP_METHODS,
+  HTTP_STATUS_CODES,
+};
