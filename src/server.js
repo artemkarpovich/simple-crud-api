fc39e98ae043aed029e8a76c0b683f0fcb35b50a
Artsiom <karpartem94@gmail.com>
2021-11-25 09:47:51 +0300

    feat: handle GET request + not found route case

--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 const http = require("http");
 const config = require("./config");
+const { HTTP_METHODS, HTTP_STATUS_CODES } = require("./constants");
+const { normalizeUrl } = require("./utils");
 const Person = require("./person");
 
-const server = http.createServer((req, res) => {
-  const persons = new Person().getPersons();
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: persons,
-    })
-  );
+const server = http.createServer(async (req, res) => {
+  if (req.method === HTTP_METHODS.GET && normalizeUrl(req.url) === "/person") {
+    try {
+      const persons = await Person.getPersons();
+
+      res.writeHead(HTTP_STATUS_CODES.OK, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          data: persons,
+        })
+      );
+    } catch (error) {
+      res.writeHead(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          error: "Something went wrong",
+        })
+      );
+    }
+  } else {
+    res.writeHead(HTTP_STATUS_CODES.NOT_FOUND, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Route not found" }));
+  }
 });
 
 server.listen(config.PORT, () => console.log(`Application is successfully running on port: ${config.PORT}`));
