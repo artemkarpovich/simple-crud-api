7b0eb1923ab7d24fe6c87292e67428e8a1b16eb6
Artsiom <karpartem94@gmail.com>
2021-11-25 10:40:25 +0300

    feat: handle get person by id route

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const http = require("http");
+const { validate } = require("uuid");
 const config = require("./config");
 const { HTTP_METHODS, HTTP_STATUS_CODES } = require("./constants");
 const { normalizeUrl } = require("./utils");
-const Person = require("./person");
+const personService = require("./person");
 
 const server = http.createServer(async (req, res) => {
-  if (req.method === HTTP_METHODS.GET && normalizeUrl(req.url) === "/person") {
+  const normalizedUrl = normalizeUrl(req.url);
+
+  if (req.method === HTTP_METHODS.GET && normalizedUrl === "/person") {
     try {
-      const persons = await Person.getPersons();
+      const persons = await personService.getPersons();
 
       res.writeHead(HTTP_STATUS_CODES.OK, { "Content-Type": "application/json" });
       res.end(
@@ -22,6 +25,31 @@ const server = http.createServer(async (req, res) => {
           error: "Something went wrong",
         })
       );
+    }
+  } else if (
+    req.method === HTTP_METHODS.GET &&
+    normalizedUrl.includes("/person/") &&
+    normalizedUrl.split("/").slice(1).length === 2
+  ) {
+    try {
+      const [, uuid] = normalizedUrl.split("/").slice(1);
+      const isValidUuid = validate(uuid);
+
+      if (!isValidUuid) {
+        throw new Error("Passed uuid ins't valid");
+      }
+
+      try {
+        const person = await personService.getPerson(uuid);
+        res.writeHead(HTTP_STATUS_CODES.OK, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ data: person }));
+      } catch (error) {
+        res.writeHead(HTTP_STATUS_CODES.NOT_FOUND, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: error.message }));
+      }
+    } catch (error) {
+      res.writeHead(HTTP_STATUS_CODES.BAD_REQUEST, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: error.message }));
     }
   } else {
     res.writeHead(HTTP_STATUS_CODES.NOT_FOUND, { "Content-Type": "application/json" });
